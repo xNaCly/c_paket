0ab677eaeff35ca247869ec2c1c748b6e9511361
xnacly <matteogropp@protonmail.com>
2022-05-01 21:36:54 +0200

    feat: WIP add color config option to util

--- src/core/c_p_util.c
+++ src/core/c_p_util.c
@@ -4,20 +4,23 @@
 
 #include "c_p_util.h"
 
+int color = 1;
 void cpak_log(const char *str, int log_level){
-    switch(log_level){
-        case SUCCESS:
-            printf("%s %s SUCCESS %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_GREEN, ANSI_RESET, str);
-            break;
-        case WARN:
-            printf("%s %s WARN %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_YELLOW, ANSI_RESET, str);
-            break;
-        case ERROR:
-            printf("%s %s ERROR %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_RED, ANSI_RESET, str);
-            break;
-        case INFO:
-        default:
-            printf("%s %s INFO %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_BLUE, ANSI_RESET, str);
+    if(color){
+        switch(log_level){
+            case SUCCESS:
+                printf("%s %s SUCCESS %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_GREEN, ANSI_RESET, str);
+                break;
+            case WARN:
+                printf("%s %s WARN %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_YELLOW, ANSI_RESET, str);
+                break;
+            case ERROR:
+                printf("%s %s ERROR %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_RED, ANSI_RESET, str);
+                break;
+            case INFO:
+            default:
+                printf("%s %s INFO %s %s\n",ANSI_COLOR_BLACK_FG, ANSI_COLOR_BLUE, ANSI_RESET, str);
+        }
     }
 }
 
