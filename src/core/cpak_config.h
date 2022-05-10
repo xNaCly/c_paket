0b138aa6a5c02d4b680686e1c212691c3a64b01e
xnacly <matteogropp@protonmail.com>
2022-05-11 00:41:58 +0200

    feat(config): merged project_config and config

--- src/core/cpak_config.h
+++ src/core/cpak_config.h
@@ -6,6 +6,15 @@ typedef struct {
     int storeModulesGlobal;
 } Config;
 
+typedef struct { char *name;
+    char *version;
+    char *cwd;
+    char **deps;
+    int amount_deps;
+} Project_config;
+
 Config *get_config(char *path);
+Project_config *project_conf_get_config(char *path);
+void project_conf_free(Project_config *conf);
 
 #endif
