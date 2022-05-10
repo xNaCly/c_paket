550691dc26c2ea4ba75fdeefe7c0c867264b1830
xnacly <matteogropp@protonmail.com>
2022-05-11 00:46:00 +0200

    docs(config): updated docs for config methods

--- src/core/cpak_config.h
+++ src/core/cpak_config.h
@@ -2,19 +2,36 @@
 #define CPAK_CONFIG_H
 
 typedef struct {
-    int colors;
-    int storeModulesGlobal;
+  int colors;
+  int storeModulesGlobal;
 } Config;
 
-typedef struct { char *name;
-    char *version;
-    char *cwd;
-    char **deps;
-    int amount_deps;
+typedef struct {
+  char *name;
+  char *version;
+  char *cwd;
+  char **deps;
+  int amount_deps;
 } Project_config;
 
+/*
+ * @brief parses config at given path
+ * @param char *path String pointing to config path
+ * @return Config pointer
+ */
 Config *get_config(char *path);
+
+/*
+ * @brief parses project config at given path
+ * @param char *path String pointing to project config
+ * @return Project_config pointer
+ */
 Project_config *project_conf_get_config(char *path);
+
+/*
+ * @brief frees given project pointer
+ * @param *conf Project_config pointer
+ */
 void project_conf_free(Project_config *conf);
 
 #endif
