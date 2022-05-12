19c743bfad1b3cd7c2cddd3819bc74c1c5929dc0
xnacly <matteogropp@protonmail.com>
2022-05-12 21:27:24 +0200

    feat(main): implemented cli argument parsing (#9)

--- src/cpak.c
+++ src/cpak.c
@@ -3,53 +3,101 @@
 #include <string.h>
 
 #include "./core/cpak_utils.h"
+#include "cpak.h"
 
 typedef enum {
-    INIT = 0,
-    INSTALL,
-    REMOVE,
-    UPGRADE,
-    BOOTSTRAP,
+  INIT = 0,
+  ADD,
+  REMOVE,
+  UPGRADE,
+  BOOTSTRAP,
+  VERSION,
+  HELP,
+  UNDEF,
+  UNKNOWN,
 } command;
 
 typedef struct {
-    command cmd; // primary argument
-    char* c_cmd; // child argument
+  command cmd; // primary argument
+  char *c_cmd; // child argument
 } Cli_arguments;
 
-Cli_arguments *parse_arguments(int arguments_amount, char** arguments){
-    Cli_arguments *ca;
-    ca = malloc(sizeof(ca));
+Cli_arguments *parse_arguments(int arguments_amount, char **arguments) {
+  Cli_arguments *ca;
+  ca = malloc(sizeof(*ca));
 
-    if(arguments_amount == 1){
-        // TODO: return usage or help
-        free(ca);
-        throw_error("Not enough Arguments", NOT_ENOUGH_ARGUMENTS);
-    }
-
-    char* cmd = arguments[1];
-
-    if(s_is_equal(cmd, "help")){
-        ca->cmd = -1;
-    } else if(s_is_equal(cmd, "bootstrap") || s_is_equal(cmd, "bo")){
-        ca->cmd = BOOTSTRAP;
-    }
-    ca->c_cmd = malloc(sizeof(arguments[2]));
-
+  if (arguments_amount == 1) {
+    ca->cmd = UNDEF;
     return ca;
+  }
+
+  char *cmd = arguments[1];
+
+  if (s_is_equal(cmd, "help"), s_is_equal(cmd, "h")) {
+    ca->cmd = HELP;
+  } else if (s_is_equal(cmd, "bootstrap") || s_is_equal(cmd, "b")) {
+    ca->cmd = BOOTSTRAP;
+  } else if (s_is_equal(cmd, "init") || s_is_equal(cmd, "i")) {
+    ca->cmd = INIT;
+  } else if (s_is_equal(cmd, "add") || s_is_equal(cmd, "a")) {
+    ca->cmd = ADD;
+  } else if (s_is_equal(cmd, "remove") || s_is_equal(cmd, "r")) {
+    ca->cmd = REMOVE;
+  } else if (s_is_equal(cmd, "upgrade") || s_is_equal(cmd, "u")) {
+    ca->cmd = UPGRADE;
+  } else if (s_is_equal(cmd, "version") || s_is_equal(cmd, "v")) {
+    ca->cmd = VERSION;
+  } else {
+    ca->cmd = UNKNOWN;
+  }
+
+  if (ca->cmd != VERSION) {
+    if (arguments_amount >= 1) {
+      ca->c_cmd = malloc(sizeof(arguments[2]));
+      ca->c_cmd = arguments[2];
+    } else {
+      throw_error("Not enough arguments, use help <command> to view in-depth "
+                  "information about <command>",
+                  NOT_ENOUGH_ARGUMENTS);
+    }
+  }
+
+  return ca;
 }
 
 int main(int argc, char *argv[]) {
-    Cli_arguments *arg = parse_arguments(argc, argv);
-    //cpak_log((char *) arg->cmd, DEBUG);
-    //cpak_log(arg->c_cmd, DEBUG);
-    switch(arg->cmd){
-        case BOOTSTRAP:
-            cpak_log("BOOTSTRAP!", DEBUG);
-            break;
-        default:
-            // TODO: see core/cpak_utils.c_l.13
-            throw_error("Unknown Argument", UNKNOWN_ARGUMENT);
-    }
-    free(arg);
+  Cli_arguments *arg = parse_arguments(argc, argv);
+
+  switch (arg->cmd) {
+  case BOOTSTRAP:
+    cpak_log("BOOTSTRAP!", DEBUG);
+    break;
+  case INIT:
+    cpak_log("INIT!", DEBUG);
+    break;
+  case ADD:
+    cpak_log("ADD!", DEBUG);
+    break;
+  case REMOVE:
+    cpak_log("REMOVE!", DEBUG);
+    break;
+  case UPGRADE:
+    cpak_log("UPGRADE!", DEBUG);
+    break;
+  case VERSION:
+    cpak_log("cpak-" CPAK_VERSION, DEBUG);
+    exit(EXIT_SUCCESS);
+  case UNDEF:
+    throw_warning("Not enough Arguments providen", NOT_ENOUGH_ARGUMENTS);
+    break;
+  case UNKNOWN:
+    throw_warning("Unknown Argument", UNKNOWN_ARGUMENT);
+    break;
+  case HELP:
+  default:
+    cpak_log("HELP!", DEBUG);
+    break;
+  }
+
+  free(arg);
 }
---
+++ src/cpak.h
@@ -0,0 +1,6 @@
+#ifndef CPAK_H
+#define CPAK_H
+
+#define CPAK_VERSION "alpha-0.0.1"
+
+#endif
