7b48c6bf2e30e87ef252fa17f1f9a4cd35ed29d0
xnacly <matteogropp@protonmail.com>
2022-05-13 14:25:45 +0200

    feat(cli): added frees to bootstraping

--- src/core/cpak_cli.c
+++ src/core/cpak_cli.c
@@ -1,52 +1,50 @@
-#include "cpak_cli.h"
-#include "cpak_utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
+#include "cpak_cli.h"
+#include "cpak_utils.h"
+
 int bootstrap(char *template_name, char *outdir) {
-    char *CONFIG_HOME = malloc(sizeof(char) * 255);
-    char *HOME_DIR = malloc(sizeof(char) * 255);
-    char *template_path = malloc(sizeof(char) * 510);
+  char *CONFIG_HOME = malloc(sizeof(char) * 255);
+  char *HOME_DIR = malloc(sizeof(char) * 255);
+  char *template_path = malloc(sizeof(char) * 510);
+  char *path = malloc(sizeof(char) * 510);
 
-    CONFIG_HOME = getenv("XDG_CONFIG_HOME");
-    HOME_DIR = getenv("HOME");
+  CONFIG_HOME = getenv("XDG_CONFIG_HOME");
+  HOME_DIR = getenv("HOME");
 
-    if(HOME_DIR == NULL || s_is_empty(HOME_DIR)){
-        strcpy(HOME_DIR, "~");
-    }
+  if (HOME_DIR == NULL || s_is_empty(HOME_DIR)) {
+    strcpy(HOME_DIR, "~");
+  }
 
-    if (CONFIG_HOME == NULL || s_is_empty(CONFIG_HOME)){
-        char *path = malloc(sizeof(char) * 510);
+  if (CONFIG_HOME == NULL || s_is_empty(CONFIG_HOME)) {
+    sprintf(path, "%s/.config", HOME_DIR);
+    CONFIG_HOME = malloc(sizeof(path));
+    strcpy(CONFIG_HOME, path);
+  }
 
-        sprintf(path, "%s/.config", HOME_DIR);
-        CONFIG_HOME = malloc(sizeof(path));
-        strcpy(CONFIG_HOME, path);
+  sprintf(template_path, "%s/cpak/templates/%s", CONFIG_HOME, template_name);
 
-        //free(path);
-    }
+  struct stat st;
+  if (stat(template_path, &st) != 0) {
+    printf("%s - ", template_path);
+    throw_error("Can't read template", TEMPLATE_MISSING);
+  }
 
-    sprintf(template_path, "%s/cpak/templates/%s", CONFIG_HOME, template_name);
-    //free(CONFIG_HOME);
+  char *command = malloc(sizeof(char) * 510);
+  sprintf(command, "cp -r %s/* %s", template_path, outdir);
 
-    struct stat st;
-    // check if directory exists
-    if (stat(template_path, &st) != 0) {
-        printf("%s - ", template_path);
-        //free(template_path);
-        throw_error("Can't read template", TEMPLATE_MISSING);
-    }
+  int r = system(command);
 
-    char *command = malloc(sizeof(char) * 510);
-    sprintf(command, "cp -r %s/* %s", template_path, outdir);
+  if (r == EXIT_SUCCESS)
+    cpak_log("Successfully bootstrapped project", SUCCESS);
 
-    //free(template_path);
-
-    int r = system(command);
-    //free(command);
-    if (r == EXIT_SUCCESS)
-        cpak_log("Successfully bootstrapped project", SUCCESS);
-
-    return r;
+  free(CONFIG_HOME);
+  free(HOME_DIR);
+  free(template_path);
+  free(path);
+  free(command);
+  return r;
 }
