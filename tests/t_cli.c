f5756757c08dfde4a97f424ccb5ef0cc75e241c8
xnacly <matteogropp@protonmail.com>
2022-05-14 15:39:29 +0200

    ci: added 't_help' to test the help cmd

--- tests/t_cli.c
+++ tests/t_cli.c
@@ -5,14 +5,27 @@
 #include "../src/core/cpak_cli.h"
 #include "../src/core/cpak_utils.h"
 
+void t_help(){
+  cpak_log("running t_help", INFO);
+  int r_value = help("all");
+  assert(r_value == EXIT_SUCCESS && "help all command failed");
+  cpak_log("help all cmd worked!", SUCCESS);
+  int r1_value = help("tester");
+  assert(r1_value == EXIT_FAILURE && "help command failed");
+  cpak_log("help cmd worked!", SUCCESS);
+  cpak_log("ran t_help", INFO);
+}
+
 void t_template() {
   cpak_log("running t_template", INFO);
   int r_value = bootstrap("default", "./tests/output");
-  assert(r_value == 0 && "bootstraping from template dir failed");
+  assert(r_value == EXIT_SUCCESS && "bootstrap default template failed");
+  cpak_log("bootstrap default template worked!", SUCCESS);
   cpak_log("ran t_template", INFO);
 }
 
 int main() {
   t_template();
+  t_help();
   return EXIT_SUCCESS;
 }
