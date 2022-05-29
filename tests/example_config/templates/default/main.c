bb567d45448e00b64f723158b34ca63f810fcb55
xnacly <matteogropp@protonmail.com>
2022-05-29 16:16:21 +0200

    feat(ci): added tests for: - c_add - c_remove - c_upgrade furthermore updated tests to display current tests in a more verbose way

--- tests/example_config/templates/default/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void){
-  // cpak default template
-  printf("Hello World from cpak!\n");
-  return EXIT_SUCCESS;
-}
--- tests/t_cli.c
+++ tests/t_cli.c
@@ -5,12 +5,68 @@
 #include "../src/core/cpak_cli.h"
 #include "../src/core/cpak_utils.h"
 
+int t_tests = 0;
+int s_tests = 0;
+int f_tests = 0;
+
+void t_add(){
+  printf("\n");
+  t_tests += 1;
+  cpak_log("running t_add", INFO);
+  int r_value = c_add("xnacly/xutils");
+  if(r_value != EXIT_SUCCESS){
+    throw_warning("add command failed", -1);
+    f_tests += 1;
+    return;
+  }
+  cpak_log("add cmd worked!", SUCCESS);
+  cpak_log("ran t_add", INFO);
+  s_tests += 1;
+}
+
+void t_upgrade(){
+  printf("\n");
+  t_tests += 1;
+  cpak_log("running t_upgrade", INFO);
+  int r_value = c_upgrade("xutils");
+  if(r_value != EXIT_SUCCESS){
+    throw_warning("upgrade command failed", -1);
+    f_tests += 1;
+    return;
+  }
+  cpak_log("upgrade cmd worked!", SUCCESS);
+  cpak_log("ran t_upgrade", INFO);
+  s_tests += 1;
+}
+
+void t_remove(){
+  printf("\n");
+  t_tests += 1;
+  cpak_log("running t_remove", INFO);
+  int r_value = c_remove("xutils");
+  if(r_value != EXIT_SUCCESS){
+    throw_warning("remove command failed", -1);
+    f_tests += 1;
+    return;
+  }
+  cpak_log("remove cmd worked!", SUCCESS);
+  cpak_log("ran t_remove", INFO);
+  s_tests += 1;
+}
+
 void t_config() {
+  printf("\n");
+  t_tests += 1;
   cpak_log("running t_config", INFO);
   int r_value = c_config();
-  assert(r_value == EXIT_SUCCESS && "config command failed");
+  if(r_value != EXIT_SUCCESS){
+    throw_warning("remove command failed", -1);
+    f_tests += 1;
+    return;
+  }
   cpak_log("config cmd worked!", SUCCESS);
   cpak_log("ran t_config", INFO);
+  s_tests += 1;
 }
 
 /* void t_init() { */
@@ -22,28 +78,69 @@ void t_config() {
 /* } */
 
 void t_help() {
+  printf("\n");
+  t_tests += 2;
   cpak_log("running t_help", INFO);
   int r_value = c_help("all");
-  assert(r_value == EXIT_SUCCESS && "help all command failed");
+  if(r_value != EXIT_SUCCESS){
+    throw_warning("help command failed", -1);
+    f_tests += 1;
+  } else {
+    s_tests += 1;
+  }
   cpak_log("help all cmd worked!", SUCCESS);
   int r1_value = c_help("tester");
-  assert(r1_value == EXIT_FAILURE && "help command failed");
+  if(r1_value == EXIT_SUCCESS){
+    throw_warning("help command failed", -1);
+    f_tests += 1;
+    return;
+  } else {
+    s_tests += 1;
+  }
+
   cpak_log("help cmd worked!", SUCCESS);
   cpak_log("ran t_help", INFO);
 }
 
 void t_template() {
+  t_tests += 1;
   cpak_log("running t_template", INFO);
   int r_value = c_bootstrap("default", "./tests/output");
-  assert(r_value == EXIT_SUCCESS && "bootstrap default template failed");
+  if(r_value != EXIT_SUCCESS){
+    throw_warning("bootstrap command failed", -1);
+    f_tests += 1;
+    return;
+  }
   cpak_log("bootstrap default template worked!", SUCCESS);
   cpak_log("ran t_template", INFO);
+  s_tests += 1;
+  printf("\n");
+}
+
+void t_clean_up(){
+  printf("\n");
+  cpak_log("Overview:", INFO);
+  char *success_tests = malloc(sizeof(char)*1024);
+  char *failed_tests = malloc(sizeof(char)*1024);
+  double success_test_percent = s_tests*100 / (double)t_tests;
+  double failed_test_percent = f_tests*100 / (double)t_tests;
+  snprintf(success_tests, 1023, "successful tests: %d/%d [%f%%]", s_tests, t_tests, success_test_percent);
+  snprintf(failed_tests, 1023, "failed tests: %d/%d [%f%%]", f_tests, t_tests, failed_test_percent);
+  cpak_log(success_tests, SUCCESS);
+  cpak_log(failed_tests, ERROR);
+  free(success_tests);
+  free(failed_tests);
 }
 
 int main() {
   /* t_init(); */
+  t_add();
+  t_upgrade();
+  t_remove();
   t_config();
   t_template();
   t_help();
+  t_clean_up();
+  if(f_tests != 0) return EXIT_FAILURE;
   return EXIT_SUCCESS;
 }
